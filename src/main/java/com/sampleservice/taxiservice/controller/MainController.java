010ba0b4555feb6fd46e7973da3096c0f87b8820
SeptemberHX <september_hx@outlook.com>
2019-12-02 16:24:49 +0800

    init

---
+++ src/main/java/com/sampleservice/taxiservice/controller/MainController.java
@@ -0,0 +1,25 @@
+package com.sampleservice.taxiservice.controller;
+
+import com.septemberhx.common.base.MResponse;
+import com.septemberhx.mclient.annotation.MApiFunction;
+import com.septemberhx.mclient.annotation.MRestApiType;
+import com.septemberhx.mclient.base.MObject;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * @author SeptemberHX
+ * @version 0.1
+ * @date 2019/11/25
+ */
+@RestController
+public class MainController extends MObject {
+    @ResponseBody
+    @RequestMapping(value = "/car", method = RequestMethod.POST)
+    @MRestApiType
+    @MApiFunction
+    public MResponse carFunction(@RequestBody MResponse requestData) {
+        MResponse result = new MResponse();
+        result.set("msg", "/car");
+        return result;
+    }
+}
